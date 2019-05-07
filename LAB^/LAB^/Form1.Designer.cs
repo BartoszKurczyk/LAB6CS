0d16e9361b546b685633794a563449fbb8780726
Bartosz Kurczyk <bartosz.kurczyk@gmail.com>
2019-05-07 22:52:31 +0200

    implementation

---
+++ LAB^/LAB^/Form1.Designer.cs
@@ -0,0 +1,159 @@
+﻿namespace LAB_
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.inputText = new System.Windows.Forms.TextBox();
+            this.firstSearched = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.threeDicTimeLabel = new System.Windows.Forms.Label();
+            this.twoDicTimeLabel = new System.Windows.Forms.Label();
+            this.readTimeLabel = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // inputText
+            // 
+            this.inputText.Location = new System.Drawing.Point(33, 31);
+            this.inputText.Name = "inputText";
+            this.inputText.Size = new System.Drawing.Size(238, 20);
+            this.inputText.TabIndex = 0;
+            this.inputText.TextChanged += new System.EventHandler(this.inputText_TextChanged);
+            // 
+            // firstSearched
+            // 
+            this.firstSearched.AutoSize = true;
+            this.firstSearched.Location = new System.Drawing.Point(145, 72);
+            this.firstSearched.Name = "firstSearched";
+            this.firstSearched.Size = new System.Drawing.Size(10, 13);
+            this.firstSearched.TabIndex = 1;
+            this.firstSearched.Text = " ";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(30, 72);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(106, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Pierwszy wyszukany:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(319, 72);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(73, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Czas odczytu:";
+            // 
+            // threeDicTimeLabel
+            // 
+            this.threeDicTimeLabel.AutoSize = true;
+            this.threeDicTimeLabel.Location = new System.Drawing.Point(541, 127);
+            this.threeDicTimeLabel.Name = "threeDicTimeLabel";
+            this.threeDicTimeLabel.Size = new System.Drawing.Size(10, 13);
+            this.threeDicTimeLabel.TabIndex = 4;
+            this.threeDicTimeLabel.Text = " ";
+            // 
+            // twoDicTimeLabel
+            // 
+            this.twoDicTimeLabel.AutoSize = true;
+            this.twoDicTimeLabel.Location = new System.Drawing.Point(541, 99);
+            this.twoDicTimeLabel.Name = "twoDicTimeLabel";
+            this.twoDicTimeLabel.Size = new System.Drawing.Size(10, 13);
+            this.twoDicTimeLabel.TabIndex = 5;
+            this.twoDicTimeLabel.Text = " ";
+            // 
+            // readTimeLabel
+            // 
+            this.readTimeLabel.AutoSize = true;
+            this.readTimeLabel.Location = new System.Drawing.Point(395, 72);
+            this.readTimeLabel.Name = "readTimeLabel";
+            this.readTimeLabel.Size = new System.Drawing.Size(10, 13);
+            this.readTimeLabel.TabIndex = 6;
+            this.readTimeLabel.Text = " ";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(319, 127);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(213, 13);
+            this.label6.TabIndex = 7;
+            this.label6.Text = "Czas tworzenia struktury dla trzech znaków:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(319, 99);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(216, 13);
+            this.label7.TabIndex = 8;
+            this.label7.Text = "Czas tworzenia struktury dla dwuch znaków:";
+            this.label7.Click += new System.EventHandler(this.label7_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.readTimeLabel);
+            this.Controls.Add(this.twoDicTimeLabel);
+            this.Controls.Add(this.threeDicTimeLabel);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.firstSearched);
+            this.Controls.Add(this.inputText);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox inputText;
+        private System.Windows.Forms.Label firstSearched;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label threeDicTimeLabel;
+        private System.Windows.Forms.Label twoDicTimeLabel;
+        private System.Windows.Forms.Label readTimeLabel;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+    }
+}
+
---
+++ LAB^/LAB^/Form1.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Diagnostics;
+
+namespace LAB_
+{
+    public partial class Form1 : Form
+    {
+        List<string> names;
+        Dictionary<string, List<string>> twoSignsDic;
+        Dictionary<string, List<string>> threeSignsDic;
+        TimeSpan readTime, threeSignsDicTime, twoSignsDicTime;
+        public Form1()
+        {
+            InitializeComponent();
+            Stopwatch timer = new Stopwatch();
+
+            timer.Start();
+            var file = new StreamReader("nazwiska.txt");
+            string line;
+            names = new List<string>();
+
+            while ((line = file.ReadLine()) != null) 
+            {
+                //System.Console.WriteLine(line);
+                var lineArray = line.Split();
+                //System.Console.WriteLine(lineArray[1]);
+                var temp = lineArray[1];
+                //System.Console.WriteLine(temp);
+                names.Add(temp);
+            }
+            timer.Stop();
+            readTime = timer.Elapsed;
+            readTimeLabel.Text = readTime.ToString();
+            timer.Reset();
+
+            timer.Start();
+            twoSignsDic = new Dictionary<string, List<string>>();
+            foreach (var name in names)
+            {
+                string twoSigns = name[0].ToString() + name[1].ToString();
+                //System.Console.WriteLine(twoSigns);
+                if(!twoSignsDic.ContainsKey(twoSigns))
+                {
+                    var tempVal = new List<string>();
+                    tempVal.Add(name);
+                    twoSignsDic[twoSigns] = tempVal;
+                }
+                else
+                {
+                    var tempVal = twoSignsDic[twoSigns];
+                    tempVal.Add(name);
+                    twoSignsDic[twoSigns] = tempVal;
+                }
+            }
+            timer.Stop();
+            twoSignsDicTime = timer.Elapsed;
+            twoDicTimeLabel.Text = twoSignsDicTime.ToString();
+            timer.Reset();
+
+            timer.Start();
+
+            threeSignsDic = new Dictionary<string, List<string>>();
+            foreach (var name in names)
+            {
+                if (name.Length > 2)
+                {
+                    string threeSigns = name[0].ToString() + name[1].ToString() + name[2].ToString();
+                    //System.Console.WriteLine(threeSigns);
+                    if (!threeSignsDic.ContainsKey(threeSigns))
+                    {
+                        var tempVal = new List<string>();
+                        tempVal.Add(name);
+                        threeSignsDic[threeSigns] = tempVal;
+                    }
+                    else
+                    {
+                        var tempVal = threeSignsDic[threeSigns];
+                        tempVal.Add(name);
+                        twoSignsDic[threeSigns] = tempVal;
+                    }
+                }
+            }
+            timer.Stop();
+            threeSignsDicTime = timer.Elapsed;
+            threeDicTimeLabel.Text = threeSignsDicTime.ToString();
+            timer.Reset();
+           
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void inputText_TextChanged(object sender, EventArgs e)
+        {
+            if(inputText.Text.Length == 0)
+            {
+                firstSearched.Text = " ";
+            }
+            else if(inputText.Text.Length==2)
+            {
+                try
+                {
+                    var temp = twoSignsDic[inputText.Text];
+                    firstSearched.Text = temp[0];
+                }
+                catch
+                {
+                    firstSearched.Text = "Nie istnieje";
+                }
+                
+            }
+            else if (inputText.Text.Length == 3)
+            {
+                try
+                {
+                    var temp = threeSignsDic[inputText.Text];
+                    firstSearched.Text = temp[0];
+                }
+                catch
+                {
+                    firstSearched.Text = "Nie istnieje";
+                }
+
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
